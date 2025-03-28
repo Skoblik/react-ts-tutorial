6fb2cfb4b2e54585a69a663f3d394ae7edc1e0a8
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-03-28 10:01:43 +0200

    Updated App with Greet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Greet } from "./components/Greet";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Greet name="Yurii" messageCount={10} />
+    </div>
+  );
 }
 
 export default App;
