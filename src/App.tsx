207624d9014f63267aa1c760465458b904f42e31
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-03-28 10:24:47 +0200

    Added Person component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import { Greet } from "./components/greet/Greet";
+import { Person } from "./components/person/Person";
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
   return (
     <div className="App">
       <Greet name="Yurii" messageCount={10} isLoggedIn={true} />
+      <Person name={personName} />
     </div>
   );
 }
