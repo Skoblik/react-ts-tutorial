8cfce0d978dbb967ebc0b72474c7f097a11bc980
Yurii Andrushchenko <yurii.andrushchenko@justanswer.com>
2025-04-11 10:24:18 +0300

    moved counter types to separate file

---
+++ src/components/state/counter/Counter.types.tsx
@@ -0,0 +1,12 @@
+export type CounterState = {
+  count: number;
+};
+
+export type UpdateAction = {
+  type: "increment" | "decrement";
+  payload: number;
+};
+
+export type ResetAction = {
+  type: "reset";
+};
